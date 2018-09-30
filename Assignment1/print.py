30e2706bf34042ada86f30fda42e462448cd8d7c
Ibraheem Tuffaha <bro.t.1996@gmail.com>
2018-09-30 22:18:14 +0300

    created the repo and added assignment1

---
+++ Assignment1/print.py
@@ -0,0 +1,4 @@
+print("Ibraheem Tuffaha")
+print("121660")
+print("'Sometimes it's the very people who no one imagines anything of who do the things that no one can imagine.'")
+print("-- Alan Turing.")
